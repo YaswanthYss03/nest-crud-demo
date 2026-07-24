3bdddbdbb3a61745c644014bbe051375ac68b6a7
Yaswanth Saran Sundar P <yaswanthsaran.sundarp2022ai-ds@sece.ac.in>
2026-07-24 21:19:44 +0530

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NotesModule } from './notes/notes.module';
+import { TodosModule } from './todos/todos.module';
+import { StudentsModule } from './students/students.module';
+import { EmployeesModule } from './employees/employees.module';
+import { BooksModule } from './books/books.module';
+import { MoviesModule } from './movies/movies.module';
+import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [NotesModule, TodosModule, StudentsModule, EmployeesModule, BooksModule, MoviesModule, ProductsModule, PrismaModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
