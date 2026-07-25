ec5d676c72f573d922760c77bfc2c19b635eb007
ShonaB29 <shonab2903@gmail.com>
2026-07-25 10:25:04 +0530

    Completed Employee CRUD using NestJS and Prisma

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,1 +1,21 @@
-export class CreateEmployeeDto {}
+import { IsString, IsEmail, IsNumber, IsDateString } from 'class-validator';
+
+export class CreateEmployeeDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  department: string;
+
+  @IsString()
+  designation: string;
+
+  @IsNumber()
+  salary: number;
+
+  @IsDateString()
+  joiningDate: string;
+}
